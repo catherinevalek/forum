ed540bd4bdc74bbb6c9d8cd435a267d6be963874
Catherine Valek <catherine.valek@gmail.com>
2017-12-20 18:02:42 -0600

    first round of models and controllers

---
+++ db/migrate/20171220234112_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string  :title, null: false
+      t.text    :text, null: false
+      t.integer :poster, null: false
+      t.timestamps
+    end
+  end
+end
