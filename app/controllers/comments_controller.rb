c427346d7c67653845e3dfe2c95161ada41e0322
Catherine Valek <catherine.valek@gmail.com>
2017-12-20 22:34:22 -0600

    add some views for posts and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,36 @@
 class CommentsController < ApplicationController
-	# before_action :find_commentable
+	before_action :find_commentable
 
- #  def new
- #    @comment = Comment.new
- #  end
+  def new
+    @comment = Comment.new
+  end
 
- #  def create
- #    @comment = @commentable.comments.new comment_params
+  def create
+    @comment = @commentable.comments.new comment_params
 
- #    if @comment.save
- #      redirect_to :back, notice: 'Your comment was successfully posted!'
- #    else
- #      redirect_to :back, notice: "Your comment wasn't posted!"
- #    end
- #  end
+    if @comment.save
+      redirect_to :back, notice: 'Your comment was successfully posted!'
+    else
+      redirect_to :back, notice: "Your comment wasn't posted!"
+    end
+  end
 
- #  private
+  def index
+		@comments = Comment.all
+  end
 
- #  def comment_params
- #    params.require(:comment).permit(:body)
- #  end
+  def show
+    @comment = Comment.find(params[:id])
+  end
 
- #  def find_commentable
- #    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
- #    @commentable = Story.find_by_id(params[:story_id]) if params[:story_id]
- #  end
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Story.find_by_id(params[:story_id]) if params[:story_id]
+  end
 end
