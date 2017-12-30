014eca5b606033896f2b9939d4693defc6e796aa
Catherine Valek <catherine.valek@gmail.com>
2017-12-29 21:02:49 -0600

    get popular posts to display on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
   	@categories = Category.all
+  	# @trending = Post.order(votes.sum(:value)).limit(5)
+  	# @trending = Vote.joins(:posts).group("votes.id").order("count(votes.id) DESC")
+@trending = Post.joins(:votes)
+  .group("posts.id, votes.value")
+  .having("votes.value > ?", 0)
+  .order("votes.value desc")
+  .where(created_at: 1.months.ago..Time.zone.now.to_date)
+  # .page(params[:page])
+  # .per_page(72)
     render :index
   end
 end
