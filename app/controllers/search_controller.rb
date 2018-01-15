fc4543bed13577730c94d31aff832d37b5a3e36d
Catherine Valek <catherine.valek@gmail.com>
2018-01-14 23:28:12 -0600

    blocked on highlighting search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
 	def index
-		@pg_search_documents = PgSearch.multisearch(params[:query]).with_pg_search_highlight
-		# binding.pry
-		p @pg_search_documents
+		@post_search_documents = PgSearch.multisearch(params[:query]).where(:searchable_type => "Post").with_pg_search_highlight
+		@comment_search_documents = PgSearch.multisearch(params[:query]).where(:searchable_type => "Comment").with_pg_search_highlight
+	  # p "postssssssssss"
+	  # p @post_search_documents
+	  # p "commentssssssssss"
+	  # p @comment_search_documents
 	end
 end
--- config/initializers/pg_search.rb
+++ config/initializers/pg_search.rb
@@ -1,5 +1,13 @@
 PgSearch.multisearch_options = {
-  using: { tsearch: { any_word: true, highlight: {
+	:kinda_matching,
+	:search_any_word,
+  using: { 	
+  	tsearch: { any_word: true, 
+  						 highlight: {
                         StartSel: '<b>',
-                        StopSel: '<b>', } }
+                        StopSel: '<b>',
+                        FragmentDelimiter: '&hellip;'
+                        HighlightAll: true
+                         } 
+                       }
 }
