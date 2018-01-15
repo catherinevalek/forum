acf57cd92e34019667ea898538ca22486498741c
Catherine Valek <catherine.valek@gmail.com>
2018-01-14 20:28:38 -0600

    first attempt at adding highlighting to searches

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
 	def index
-		@pg_search_documents = PgSearch.multisearch(params[:query])
+		@pg_search_documents = PgSearch.multisearch(params[:query]).with_pg_search_highlight
+		# binding.pry
+		p @pg_search_documents
 	end
 end
