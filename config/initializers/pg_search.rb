d3f0bb6d86e0e353d683e5ac594d51f649e9430c
Catherine Valek <catherine.valek@gmail.com>
2018-01-15 18:25:49 -0600

    adjust navbar and topics display

--- config/initializers/pg_search.rb
+++ config/initializers/pg_search.rb
@@ -1,13 +1,21 @@
+# PgSearch.multisearch_options = {
+# 	# :kinda_matching,
+# 	# :search_any_word,
+#   using: { 	
+#   	tsearch: { any_word: true, 
+#   						 highlight: {
+#                         StartSel: '<b>',
+#                         StopSel: '<b>',
+#                         HighlightAll: true,
+#                         # FragmentDelimiter: '&hellip;'
+#                         } 
+#                        }
+# }
+
+
 PgSearch.multisearch_options = {
-	:kinda_matching,
-	:search_any_word,
-  using: { 	
-  	tsearch: { any_word: true, 
-  						 highlight: {
-                        StartSel: '<b>',
-                        StopSel: '<b>',
-                        FragmentDelimiter: '&hellip;'
-                        HighlightAll: true
-                         } 
-                       }
-}
+	using: { tsearch: { any_word: true, highlight: {
+		StartSel: '<b>',
+		StopSel: '</b>',
+		FragmentDelimiter: '&hellip;' } }
+		}}
